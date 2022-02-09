0b0899218f1674958f31a426a4a08e2694be3369
tal.hayun2010@gmail.com <tal.hayun2010@gmail.com>
2022-02-09 11:32:08 +0200

    added loaded albums list

--- album.py
+++ album.py
@@ -1,5 +1,6 @@
 class Album(object):
-    def __init__(self, id, name, tracks: []):
+    def __init__(self, id, name, tracks: [], num_of_songs):
         self.id = id
         self.name = name
         self.tracks = tracks
+        self.num_of_songs = num_of_songs
---
+++ read_and_load/load.py
@@ -0,0 +1,53 @@
+import json
+import os
+import glob
+import pprint
+
+from album import Album
+from artist import Artist
+from track import Track
+
+# Reading the files from the folder and loading the json object into my costum python objects.
+
+trackList = []
+loaded_albums = []  # list of albums
+# todo: config path
+path = 'C:/Users/Tal/Desktop/Course/Spotipy/songs'
+for filename in glob.glob(os.path.join(path, '*.json')):  # only process .JSON files in folder.
+    with open(filename, encoding='utf-8', mode='r') as currentFile:
+        data = currentFile.read().replace('\n', '')
+        single_track = json.loads(data)['track']
+        if single_track not in trackList:
+            trackList.append(single_track)
+
+
+def album_in_loaded(id):
+    for album in loaded_albums:
+        if (album.id == id):
+            return album
+    return -1
+
+
+# to objects
+track_obj_list = []
+for track in trackList:
+    artists = []
+    for artist in track["artists"]:
+        artists.append(Artist(artist["id"], artist["name"]))
+    if album_in_loaded(track["album"]["id"]) != -1:
+        track_obj_list.append(
+            Track(track["id"], album_in_loaded(track["album"]["id"]), artists, track["popularity"]))
+        album_in_loaded(track["album"]["id"]).num_of_songs = album_in_loaded(track["album"]["id"]).num_of_songs + 1
+    else:  # album is unknown
+        loaded_albums.append(Album(track["album"]["id"], track["album"]["name"], track, 1))
+
+
+def main():
+    pp = pprint.PrettyPrinter(depth=6)
+    pp.pprint(trackList[:4])
+    for album in loaded_albums:
+        print(album.__dict__)
+
+
+if __name__ == '__main__':
+    main()
